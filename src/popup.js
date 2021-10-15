1186deba30069846df452e797ed531ca65249ba8
Praveen Kumar G <praveengopi16@gmail.com>
2021-10-15 23:35:08 +0530

    feat: added eslint  (#4)
    * fix: ui changes (removed form layout)
    
    * added gitignore
    
    * typo gitigonre
    
    * added eslint

--- src/popup.js
+++ src/popup.js
@@ -1,137 +1,143 @@
-let urls = {};
-
-let tempUrls = new Set();
+const urls = {},
+    tempUrls = new Set();
 
 function createLists(tabs) {
+    const fragment = document.createDocumentFragment();
 
-  let fragment = document.createDocumentFragment();
+    tabs.forEach((tab) => {
+        const element = document.createElement('li');
+        element.classList.add('individual-tab');
 
-  tabs.forEach((tab, index) => {
-    let element = document.createElement("li");
-    element.classList.add("individual-tab")
+        const labelChild = document.createElement('label');
+        labelChild.classList.add('tab-label');
 
-    let labelChild = document.createElement("label");
-    labelChild.classList.add("tab-label");
+        const inputChild = document.createElement('input');
+        inputChild.classList.add('tab-input');
+        inputChild.type = 'checkbox';
+        inputChild.id = tab.id;
 
-    let inputChild = document.createElement("input");
-    inputChild.classList.add("tab-input");
-    inputChild.type = "checkbox";
-    inputChild.id = tab.id;
+        const titleChild = document.createElement('div');
+        titleChild.classList.add('tab-title');
+        titleChild.textContent = tab.title;
 
-    let titleChild = document.createElement("div");
-    titleChild.classList.add("tab-title");
-    titleChild.textContent = tab.title
+        const urlChild = document.createElement('small');
+        urlChild.classList.add('tab-url');
+        urlChild.textContent = tab.url;
 
-    let urlChild = document.createElement("small")
-    urlChild.classList.add("tab-url")
-    urlChild.textContent = tab.url
+        labelChild.appendChild(inputChild);
+        labelChild.appendChild(titleChild);
+        labelChild.appendChild(urlChild);
 
-    // labelChild.htmlFor = tab.id;
+        element.appendChild(labelChild);
 
-    labelChild.appendChild(inputChild);
-    labelChild.appendChild(titleChild);
-    labelChild.appendChild(urlChild);
+        fragment.appendChild(element);
 
-    // element.appendChild(inputChild);
-    element.appendChild(labelChild);
+        urls[tab.id] = { title: tab.title, url: tab.url };
+    });
 
-    fragment.appendChild(element);
-
-    urls[tab.id] = { title: tab.title, url: tab.url };
-  });
-
-  document.getElementById("tab-group").appendChild(fragment);
+    document.getElementById('tab-group').appendChild(fragment);
 }
 
 chrome.tabs.query({ currentWindow: true }, createLists);
 
-window.onload = function () {
-  const form = document.getElementById("submit");
-
-  form.addEventListener("click", download);
-
-  document.getElementById("tab-group").addEventListener("change", handleChange);
-
-  document
-    .getElementById("selectAll")
-    .addEventListener("change", handleSelectAll);
-};
-
-function handleSelectAll(event) {
-  event.stopPropagation();
-
-  let sizeOfTempUrl = tempUrls.size;
-  console.log(sizeOfTempUrl);
-  if (sizeOfTempUrl != 0 && sizeOfTempUrl <= Object.keys(urls).length) {
-    tempUrls.clear();
-    document.getElementById("selectAll").checked = false;
-
-    Object.keys(urls).forEach((id) => {
-      document.getElementById(id).checked = false;
-    });
-  } else {
-    console.log("Fill");
-    Object.keys(urls).forEach((id) => {
-      tempUrls.add(id);
-      document.getElementById(id).checked = true;
-    });
-    document.getElementById("selectAll").checked = true;
-  }
-
-  handleSelectedInfo()
+function handleSelectedInfo() {
+    const tempUrlsSize = tempUrls.size;
+    if (tempUrlsSize) {
+        const countNode = document.getElementById('selectedCount');
+        countNode.classList.remove('selected-count-hide');
+        if (tempUrlsSize == 1) {
+            countNode.textContent = '1 Tab selected';
+        } else {
+            countNode.textContent = `${tempUrlsSize} Tabs selected`;
+        }
+    } else {
+        const countNode = document.getElementById('selectedCount');
+        countNode.classList.add('selected-count-hide');
+    }
 }
 
-function handleSelectedInfo(){
-  const tempUrlsSize = tempUrls.size
-  if(tempUrlsSize){
-    const countNode = document.getElementById("selectedCount")
-    countNode.classList.remove("selected-count-hide")
-    if(tempUrlsSize==1){
-      countNode.textContent = "1 Tab selected"
-    }else{
-      countNode.textContent = `${tempUrlsSize} Tabs selected`
+
+function handleSelectAll(event) {
+    event.stopPropagation();
+
+    const sizeOfTempUrl = tempUrls.size;
+    if (sizeOfTempUrl != 0 && sizeOfTempUrl <= Object.keys(urls).length) {
+        tempUrls.clear();
+        document.getElementById('selectAll').checked = false;
+
+        Object.keys(urls).forEach((id) => {
+            document.getElementById(id).checked = false;
+        });
+    } else {
+        Object.keys(urls).forEach((id) => {
+            tempUrls.add(id);
+            document.getElementById(id).checked = true;
+        });
+        document.getElementById('selectAll').checked = true;
     }
-  }else{
-    const countNode = document.getElementById("selectedCount")
-    countNode.classList.add("selected-count-hide")
-  }
+
+    handleSelectedInfo();
 }
 
 function handleChange(event) {
-  event.stopPropagation();
+    event.stopPropagation();
 
-  const { id } = event.target;
+    const { id } = event.target;
 
-  let newId = id;
+    const newId = id;
 
-  if (urls[newId] && tempUrls.has(newId)) {
-    tempUrls.delete(newId);
-  } else {
-    tempUrls.add(newId);
-  }
+    if (urls[newId] && tempUrls.has(newId)) {
+        tempUrls.delete(newId);
+    } else {
+        tempUrls.add(newId);
+    }
 
-  handleSelectedInfo()
+    const sizeOfTempUrl = tempUrls.size;
 
+    if (sizeOfTempUrl == Object.keys(urls).length) {
+        document.getElementById('selectAll').checked = true;
+        document.getElementById('selectAll').indeterminate = false;
+    } else if (sizeOfTempUrl === 0) {
+        document.getElementById('selectAll').checked = false;
+        document.getElementById('selectAll').indeterminate = false;
+    } else {
+        document.getElementById('selectAll').indeterminate = true;
+    }
+
+    handleSelectedInfo();
 }
 
 function download(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  let newUrls = Array.from(tempUrls);
+    const newUrls = Array.from(tempUrls);
 
-  var data = "<html><head></head><body>";
-  for (var i = 0; i < newUrls.length; i++) {
-    const title = urls[newUrls[i]].title;
-    const url = urls[newUrls[i]].url;
-    data += '<a href="' + url + '">' + title + "</a><br/>";
-  }
-  data += "</body></html>";
+    let data = '<html><head></head><body>';
+    for (let i = 0; i < newUrls.length; i++) {
+        const { title } = urls[newUrls[i]];
+        const { url } = urls[newUrls[i]];
+        data += `<a href="${url}">${title}</a><br/>`;
+    }
+    data += '</body></html>';
 
-  var blob = new Blob([data], { type: "text/html;charset=utf-8" });
-  var url = URL.createObjectURL(blob);
-  var a = document.createElement("a");
+    const blob = new Blob([data], { type: 'text/html;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
 
-  a.download = "tabs.html";
-  a.href = url;
-  a.click();
+    a.download = 'tabs.html';
+    a.href = url;
+    a.click();
 }
+
+
+window.onload = function () {
+    const submitButton = document.getElementById('submit');
+
+    submitButton.addEventListener('click', download);
+
+    document.getElementById('tab-group').addEventListener('change', handleChange);
+
+    document
+        .getElementById('selectAll')
+        .addEventListener('change', handleSelectAll);
+};
